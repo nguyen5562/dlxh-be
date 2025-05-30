c1fc25703bc2be1f5b063317c1145ae5164f2c6a
nguyen5562 <nknguyen112345@gmail.com>
2025-05-30 09:10:42 +0700

    fix guard

--- src/modules/quyen/quyen.module.ts
+++ src/modules/quyen/quyen.module.ts
@@ -1,12 +1,14 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Quyen, QuyenSchema } from './schema/quyen.schema';
 import { QuyenService } from './quyen.service';
 import { QuyenController } from './quyen.controller';
+import { NguoiDungModule } from '../nguoi-dung/nguoi-dung.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Quyen.name, schema: QuyenSchema }]),
+    forwardRef(() => NguoiDungModule),
   ],
   controllers: [QuyenController],
   providers: [QuyenService],
